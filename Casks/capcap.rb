be40de073a88035aa31a1c4d0362cdd17d42dde7
luoyanlin <cnskyrin@gmail.com>
2026-05-24 12:43:21 +0800

    Bump cask to 1.3.16

--- Casks/capcap.rb
+++ Casks/capcap.rb
@@ -1,6 +1,6 @@
 cask "capcap" do
-  version "1.3.15"
-  sha256 "050dae4f15112d8c961cca690d23b1952ef0efab74658fd0844a9402530fa758"
+  version "1.3.16"
+  sha256 "20e2dd7a2b0d2d87ef1ed3d3edef2721285b06ced22e1e4ee501a7762d771c20"
 
   url "https://github.com/realskyrin/capcap/releases/download/release-v#{version}/capcap-#{version}-macos.zip"
   name "capcap"
